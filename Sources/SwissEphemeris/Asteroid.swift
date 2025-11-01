d99126786ec3d6b97a48d3c25af6f0645f570256
bangledev <239207871+bangledev@users.noreply.github.com>
2025-11-01 23:39:24 +0100

    add muses

--- Sources/SwissEphemeris/Asteroid.swift
+++ Sources/SwissEphemeris/Asteroid.swift
@@ -11,13 +11,15 @@ import CSwissEphemeris
 /// Models an asteroid.
 public struct Asteroid: CelestialBody, Hashable {
     public static var allCases: [Asteroid] = [
-        .chiron,
-        .pholus,
-        .ceres,
-        .pallas,
-        .juno,
-        .vesta,
-        .hygiea
+        .melpomene,
+        .calliope,
+        .thalia,
+        .euterpe,
+        .urania,
+        .polyhymnia,
+        .erato,
+        .terpsichore,
+        .clio,
     ]
 
     public let name: String
@@ -40,12 +42,14 @@ public struct Asteroid: CelestialBody, Hashable {
 
 // MARK: - Predefined Asteroids
 public extension Asteroid {
-    static let chiron = Asteroid(name: "Chiron", value: SE_CHIRON)
-    static let pholus = Asteroid(name: "Pholus", value: SE_PHOLUS)
-    static let ceres = Asteroid(name: "Ceres", value: SE_CERES)
-    static let pallas = Asteroid(name: "Pallas", value: SE_PALLAS)
-    static let juno = Asteroid(name: "Juno", value: SE_JUNO)
-    static let hygiea = Asteroid.numbered(10, name: "Hygiea")
-    static let mimosa = Asteroid.numbered(1079, name: "Mimosa")
-    static let vesta = Asteroid(name: "Vesta", value: SE_VESTA)
+    static let melpomene = Asteroid.numbered(18, name: "Melpomene")
+    static let calliope = Asteroid.numbered(22, name: "Calliope")
+    static let thalia = Asteroid.numbered(23, name: "Thalia")
+    static let euterpe = Asteroid.numbered(27, name: "Euterpe")
+    static let urania = Asteroid.numbered(30, name: "Urania")
+    static let polyhymnia = Asteroid.numbered(33, name: "Polyhymnia")
+    static let erato = Asteroid.numbered(62, name: "Erato")
+    static let terpsichore = Asteroid.numbered(81, name: "Terpsichore")
+    static let clio = Asteroid.numbered(84, name: "Clio")
+
 }
